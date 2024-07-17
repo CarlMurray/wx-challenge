bc5b6b651b566a4e066e0d34584a838dd73ef155
CarlMurray <carl.j.murray@gmail.com>
2024-07-17 11:49:15 +0100

    chore: Update openapi-spec.json to version 3.0.0 and add security scheme for bearer authentication

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask_cors import CORS
+import requests
+import json
+import markdown
+from flask import request
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/workday/api/get_user_context")
 def get_user_context():
     print("app is running")
     return {
@@ -22,6 +25,56 @@ def get_user_context():
             "email": "thomas.watson@ibm.com"
         }
     }
+    
+# Models
+GRANITE_CHAT = "ibm/granite-13b-chat-v2"
+GRANITE_INSTRUCT = "ibm/granite-13b-instruct-v2"
+LLAMA_3_INSTRUCT = "meta-llama/llama-3-70b-instruct"
+LLAMA_2_CHAT = "meta-llama/llama-2-70b-chat"
+MIXTRAL_INSTRUCT = "mistralai/mixtral-8x7b-instruct-v01"
+
+# Model parameters
+MAX_NEW_TOKENS = 2500
+MIN_NEW_TOKENS = 200
+REPETITION_PENALTY = 1
+
+# API_KEY=os.environ.get("API_KEY")
+API_KEY='BGthpi04Zy0luvLxtPhGvYqspGYSpU1Ae_sjOXF2MShX'
+API_URL_ENDPOINT = "https://us-south.ml.cloud.ibm.com/ml/v1/text/generation?version=2024-06-01"
+INSTRUCTION = """You are a security AI assistant. You will take the JSON data about an employee and generate a human readable summary of the data."""
+
+@app.route("/workday/api/get_user_context")
+def summarise():
+    token = get_iam_token()
+    jsondata = get_user_context()
+    headers = {"Authorization": f'Bearer {token}', 
+                       "content-type":"application/json"}
+    data = {
+	  "model_id": MIXTRAL_INSTRUCT,
+    #   "input": (INSTRUCTION+ "<|start_header_id|>user<|end_header_id|>"+jsondata["input"]+"<|eot_id|><|start_header_id|>assistant<|end_header_id|>"),
+      "input": (INSTRUCTION+jsondata),
+      "project_id": "fc373787-aaaa-4622-93df-65e71b0a579f",
+      	"parameters": {
+		"decoding_method": "greedy",
+		"max_new_tokens": MAX_NEW_TOKENS,
+		"repetition_penalty": REPETITION_PENALTY,
+        "stop_sequences": [],
+	    },
+    }
+    r = requests.post(url=API_URL_ENDPOINT, json=data, headers=headers)
+    print(r)
+    jsondata = r.json()
+    print(jsondata)
+    return markdown.markdown(jsondata["results"][0]["generated_text"])
+
+def get_iam_token():
+    url = 'https://iam.cloud.ibm.com/identity/token'
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    data = f'grant_type=urn:ibm:params:oauth:grant-type:apikey&apikey={API_KEY}'
+    r = requests.post(url=url, data=data, headers=headers)
+    token = r.json()
+    token = token["access_token"]
+    return token
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
