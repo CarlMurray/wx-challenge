8ce3f3140444a4705085cc6f5144b41102f292a5
CarlMurray <carl.j.murray@gmail.com>
2024-07-18 17:15:53 +0100

    fix

--- app.py
+++ app.py
@@ -9,8 +9,6 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-API_KEY = 'z-GpPa-RYcdeuW3vCir7P9608eATlTESgIu4lpBPTOyw'
-
 def get_user_context():
     print("app is running")
     return {
@@ -156,7 +154,7 @@ MIN_NEW_TOKENS = 200
 REPETITION_PENALTY = 1
 
 # API_KEY=os.environ.get("API_KEY")
-API_KEY='BGthpi04Zy0luvLxtPhGvYqspGYSpU1Ae_sjOXF2MShX'
+# API_KEY='BGthpi04Zy0luvLxtPhGvYqspGYSpU1Ae_sjOXF2MShX'
 API_URL_ENDPOINT = "https://us-south.ml.cloud.ibm.com/ml/v1/text/generation?version=2024-06-01"
 INSTRUCTION = """<|begin_of_text|><|start_header_id|>system<|end_header_id|>
 
@@ -178,24 +176,24 @@ def index():
 
 @app.route("/workday/api/get_user_context")
 def summarise():
-    token = get_iam_token()
+    # token = get_iam_token()
     user_context_data = get_user_context()
     user_context_data_string = json.dumps(user_context_data)
-    headers = {"Authorization": f'Bearer {token}', 
-                       "content-type":"application/json"}
-    data = {
-	  "model_id": LLAMA_3_INSTRUCT,
-    #   "input": (INSTRUCTION+ "<|start_header_id|>user<|end_header_id|>"+jsondata["input"]+"<|eot_id|><|start_header_id|>assistant<|end_header_id|>"), 
-    #   "input": (INSTRUCTION+user_context_data_string),
-        "input":INSTRUCTION + "<|start_header_id|>user<|end_header_id|>"+ user_context_data_string + "<|eot_id|><|start_header_id|>assistant<|end_header_id|>",
-        "project_id": "fc373787-aaaa-4622-93df-65e71b0a579f",
-      	"parameters": {
-		"decoding_method": "greedy",
-		"max_new_tokens": MAX_NEW_TOKENS,
-		"repetition_penalty": REPETITION_PENALTY,
-        "stop_sequences": [],
-	    },
-    }
+    # headers = {"Authorization": f'Bearer {token}', 
+    #                    "content-type":"application/json"}
+    # data = {
+	  # "model_id": LLAMA_3_INSTRUCT,
+    # #   "input": (INSTRUCTION+ "<|start_header_id|>user<|end_header_id|>"+jsondata["input"]+"<|eot_id|><|start_header_id|>assistant<|end_header_id|>"), 
+    # #   "input": (INSTRUCTION+user_context_data_string),
+    #     "input":INSTRUCTION + "<|start_header_id|>user<|end_header_id|>"+ user_context_data_string + "<|eot_id|><|start_header_id|>assistant<|end_header_id|>",
+    #     "project_id": "fc373787-aaaa-4622-93df-65e71b0a579f",
+    #   	"parameters": {
+		# "decoding_method": "greedy",
+		# "max_new_tokens": MAX_NEW_TOKENS,
+		# "repetition_penalty": REPETITION_PENALTY,
+    #     "stop_sequences": [],
+	  #   },
+    # }
     # r = requests.post(url=API_URL_ENDPOINT, json=data, headers=headers)
     # jsondata = r.json()
     # summary = jsondata["results"][0]["generated_text"]
